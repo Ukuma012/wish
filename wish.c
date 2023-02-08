183c8f2f3437e8ef3c92ca87c42132f7344ebd87
Ukuma012 <ukuma012@gmail.com>
2023-02-08 14:18:15 +0900

    :art: fork added

--- wish.c
+++ wish.c
@@ -3,30 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-// BUGGY!!!!!!!!!
-
 int main (int argc, char *argv[]) {
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
+
   while(1) {
     printf("wish> ");
     if((linelen = getline(&line, &linecap, stdin)) > 0) {
       if(strcmp(line, "exit\n") == 0) {
 	exit(0);
       } 
-      printf("hello world (pid:%d)\n", (int) getpid());
       int rc = fork();
       if(rc < 0) {
 	fprintf(stderr, "fork failed");
 	exit(1);
       }else if(rc == 0) {
-	printf("hello I am child (pid:%d)\n", (int) getpid());
+	fwrite(line, linelen, 1, stdout);
+	exit(0);
       } else {
-	printf("hello I am parent of %d (pid:%d)\n", rc, (int) getpid());
+      	wait(NULL);
       }
-      fwrite(line, linelen, 1, stdout);
-      line = NULL;
     }
   }
   return 0;
