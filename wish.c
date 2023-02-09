ad3b45d87f537667838568ea3cd0216caf2e3478
Ukuma012 <ukuma012@gmail.com>
2023-02-09 10:11:15 +0900

    :art: access system call added

--- wish.c
+++ wish.c
@@ -7,11 +7,18 @@ int main (int argc, char *argv[]) {
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
+  char error_message[30] = "An error has occurred\n";
 
   while(1) {
     printf("wish> ");
     if((linelen = getline(&line, &linecap, stdin)) > 0) {
-      if(strcmp(line, "exit\n") == 0) {
+      // 末尾の改行を終端に変更
+      char *p;
+      p = strchr(line, '\n');
+      if(p != NULL) {
+	*p = '\0';
+      }
+      if(strcmp(line, "exit") == 0) {
 	exit(0);
       } 
       int rc = fork();
@@ -19,22 +26,31 @@ int main (int argc, char *argv[]) {
 	fprintf(stderr, "fork failed\n");
 	exit(1);
       }else if(rc == 0) {
-	// ここでコマンドを実行
-	// /bin/lsが引数を取らない場合、後にスペースが必要になっている
-	// /bin/ls ./testsは成功
+        // cat wish.cのような感じで動くようになった
+	// 動的なargs配列確保をすれば良い？
+
+	char *args[3];
+	char dst[20];
 	char *string;
-	char *myargs[3];
 	int i = 0;
 	while((string = strsep(&line, " ")) != NULL) {
-	  myargs[i] = string;
-	  i++;
+		args[i] = string;
+		i++;
 	}
-	//myargs[1] = "./tests";
-	myargs[2] = NULL;
-	printf("%s", myargs[0]);
-	if((execv(myargs[0], myargs)) < 0) {
-		fprintf(stderr, "execv failed\n");
-		exit(1);
+	args[i] = NULL;
+
+	snprintf(dst, sizeof(dst), "/bin/%s", args[0]);
+	if((access(dst, X_OK)) < 0) {
+	  printf("cannot access /bin/\n");
+	  snprintf(dst, sizeof(dst), "/usr/bin/%s", args[0]);
+	  if((access(dst, X_OK)) < 0) {
+	    printf("cannot access /usr/bin/\n");
+	  }
+	}
+	if((execv(dst, args)) < 0) {
+	  printf("%s\n", dst);
+	  fprintf(stderr, "execv failed\n");
+	  exit(1);
 	}
 	exit(0);
       } else {
