c6404f73c7453231e07b385b2e793a5fda4593d7
Ukuma012 <ukuma012@gmail.com>
2023-02-14 13:37:27 +0900

    buffer version

--- wish_parse.c
+++ wish_parse.c
@@ -3,25 +3,45 @@
 
 #define BUFSIZE 100
 
+char *get(char *, int);
+int getcmd(char *, int);
+
 // testcommand\0
 
-int main(int argc, char *argv[]) {
-  char buf[BUFSIZE];
-  int c;
-  int i;
-  for(i = 0; i+1 < BUFSIZE; ){
+char *get(char *buf, int max) {
+  int c, i;
+  for(i = 0; i+1 < max; ) {
     c = fgetc(stdin);
     if(c == '\n')
       break;
-    if(c < 0) {
-      fprintf(stderr, "fgets failed\n");
-      exit(1);
-    }
+    if(c < 0) 
+      break;
     buf[i++] = c;
   }
   buf[i] = '\0';
-  for(int j = 0; j < i+1; j++) {
-    printf("%c\n", buf[j]);
+  return buf;
+}
+
+int getcmd(char *buf, int nbuf) {
+  printf("%s", "wish> ");
+  get(buf, nbuf);
+  if(buf[0] == 0)
+    return -1;
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  char buf[BUFSIZE];
+  while(getcmd(buf, sizeof(buf)) >= 0) {
+    if(buf[0] == 'e' && buf[1] == 'x' && buf[2] == 'i' && buf[3] == 't') {
+      exit(0);
+    }
+    for(int j = 0; j < BUFSIZE; j++) {
+      if(buf[j] == '\0') 
+        break;
+      printf("%c", buf[j]);
+    }
+    printf("\n");
   }
   return 0;
- }
+}
